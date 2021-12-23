fbe3fd45557bb040956083afe366133002ee97df
RafaelAlmeida08 <rafaelalmeida.08@hotmail.com>
2021-12-23 20:56:49 -0300

    ajustando port

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@ import './infrastructure/database';
 import { routes } from './routes';
 
 server.use('/', routes)
-server.listen( 5000 , () => console.log('Server is running on port 5000'));
+server.listen( process.env.PORT || 5000 , () => console.log('Server is running on port 5000'));
 
 
 
