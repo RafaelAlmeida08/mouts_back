d9700cbeb6081fbd57c5bdcd74e0b890e005d850
RafaelAlmeida08 <rafaelalmeida.08@hotmail.com>
2021-12-23 23:14:20 -0300

    alterando migration

--- src/infrastructure/database/migrations/1640311696608-MudandoTimestamp.ts
+++ src/infrastructure/database/migrations/1640311696608-MudandoTimestamp.ts
@@ -9,6 +9,7 @@ export class MudandoTimestamp1640311696608 implements MigrationInterface {
         await queryRunner.query(`CREATE TABLE \`localidades\` (\`id\` varchar(255) NOT NULL, \`nome\` varchar(100) NOT NULL, \`endereco\` varchar(255) NOT NULL, \`created_at\` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP, \`localidade_tipo\` int NULL, \`cidade\` varchar(255) NULL, PRIMARY KEY (\`id\`)) ENGINE=InnoDB`);
         await queryRunner.query(`ALTER TABLE \`localidades\` ADD CONSTRAINT \`FK_c6a14fd0d7b31d36550bf554a48\` FOREIGN KEY (\`localidade_tipo\`) REFERENCES \`tipos\`(\`id\`) ON DELETE CASCADE ON UPDATE NO ACTION`);
         await queryRunner.query(`ALTER TABLE \`localidades\` ADD CONSTRAINT \`FK_a2a1329aaac729ffc4ca4bbb3ce\` FOREIGN KEY (\`cidade\`) REFERENCES \`cidades\`(\`id\`) ON DELETE NO ACTION ON UPDATE NO ACTION`);
+        await queryRunner.query("INSERT INTO \`tipos`(`id`, `nome`) VALUES (1,'Comercial'), (2, 'Residencial'), (3, 'Outros')");
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
