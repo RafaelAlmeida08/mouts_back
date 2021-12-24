7ec837178c068eabf4b4f383d6e1d2969daca688
RafaelAlmeida08 <rafaelalmeida.08@hotmail.com>
2021-12-24 19:02:50 -0300

    documentando a api

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -4,11 +4,17 @@ import { cidadesRoutes } from '../app/modules/client/cidades/routes';
 import { localidadesRoutes } from '../app/modules/client/localidades/routes';
 import { notasRoutes } from '../app/modules/client/notas/routes';
 
+import SwaggerUi from 'swagger-ui-express';
+import swaggerDocs from '../docs/swagger.json';
+
+
 const routes = Router();
 
 routes.use('/cidades', cidadesRoutes);
 routes.use('/localidades', localidadesRoutes);
 routes.use('/notas', notasRoutes);
 routes.use('/categorias', categoriasRoutes);
+
+routes.use('/docs', SwaggerUi.serve, SwaggerUi.setup(swaggerDocs))
 
 export { routes } 
